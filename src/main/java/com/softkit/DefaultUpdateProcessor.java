ceb308627f9d8ca525ecdfa4e4d3f5ba68fa5ec4
saloed <bogdan19021998ua@gmail.com>
2019-12-16 13:24:31 +0200

    Removed constructor. Added for all fields @Autowired. Added for messageSender @Qualifier("botMessageSender").

--- src/main/java/com/softkit/DefaultUpdateProcessor.java
+++ src/main/java/com/softkit/DefaultUpdateProcessor.java
@@ -3,14 +3,13 @@ package com.softkit;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.softkit.database.User;
-import lombok.RequiredArgsConstructor;
 import com.softkit.repository.UserRepository;
 import com.softkit.steps.AbstractStep;
 import com.softkit.steps.StepHolder;
 import com.softkit.vo.UpdateProcessorResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
@@ -18,25 +17,31 @@ import java.util.Optional;
 @Component
 public class DefaultUpdateProcessor implements UpdateProcessor {
 
-    //@Autowired
-    private final UserRepository userRepository;
-    private final StepHolder stepHolder;
-    private final IMessageSender messageSender;
+    @Autowired
+    private UserRepository userRepository;
 
-    public DefaultUpdateProcessor(UserRepository userRepository, StepHolder stepHolder, IMessageSender messageSender) {
-        this.userRepository = userRepository;
-        this.stepHolder = stepHolder;
-        this.messageSender = messageSender;
-    }
+    @Autowired
+    private StepHolder stepHolder;
+
+    @Autowired
+    @Qualifier("botMessageSender")
+    private IMessageSender messageSender;
+
+//    public DefaultUpdateProcessor(UserRepository userRepository, StepHolder stepHolder, IMessageSender messageSender) {
+//        this.userRepository = userRepository;
+//        this.stepHolder = stepHolder;
+//        this.messageSender = messageSender;
+//    }
 
     @Override
     public void process(Update update) {
+
 
         Optional<User> user = userRepository.findUserByUserId(getUserId(update));
 
         AbstractStep step = user.map(u -> stepHolder.getStep(u.getCurrentStep())).orElseGet(stepHolder::getDefaultStatus);
 
-        UpdateProcessorResult result = step.process(update, user.orElse(null));
+        UpdateProcessorResult result = step.process(update, user.orElse( new User() ));
 
         if(step.getStepId() == result.getNextStep()) {
             messageSender.send(result.getRequest());
@@ -51,4 +56,5 @@ public class DefaultUpdateProcessor implements UpdateProcessor {
     public Integer getUserId(Update u) {
         return 1;
     }
+
 }
