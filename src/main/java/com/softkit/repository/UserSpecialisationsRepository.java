57d81f5cecc9dd36d89117304e0ad96c5d2d965d
saloed <bogdan19021998ua@gmail.com>
2019-12-25 15:46:59 +0200

    Changed UserSpecialisationsRepository ( added method remove Specialization ).

--- src/main/java/com/softkit/repository/UserSpecialisationsRepository.java
+++ src/main/java/com/softkit/repository/UserSpecialisationsRepository.java
@@ -1,14 +1,19 @@
 package com.softkit.repository;
 
 import com.softkit.database.UserSpecialization;
+import com.softkit.vo.Specialization;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface UserSpecialisationsRepository extends CrudRepository<UserSpecialization, Integer> {
 
-    default void addUserSpecialisation(Integer userId, String specialisationDescription){};
-
-    default void removeUserSpecialisation(Integer userId, String specialisationDescription){};
-
+    @Query(value = "delete from UserSpecialization us where us.userId = :userId and us.specialization = :specialization")
+    @Modifying
+    @Transactional
+    Integer removeUserSpecialization(Integer userId, Specialization specialization);
 }
