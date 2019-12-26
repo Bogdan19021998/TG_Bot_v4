995bf43c83cf6d5841b133e228f6ae5bf4633c20
saloed <bogdan19021998ua@gmail.com>
2019-12-26 11:08:08 +0200

    Implements interface UserFieldsSetter for UserService and redefined setters.

--- src/main/java/com/softkit/service/UserService.java
+++ src/main/java/com/softkit/service/UserService.java
@@ -14,20 +14,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserService {
+public class UserService implements UserFieldsSetter {
 
     private final UserRepository userRepository;
     private final LocationRepository locationRepository;
+
     private final EmploymentsService employmentsService;
     private final ReferralService referralService;
     private final SpecializationService specializationService;
     private final TechnologiesService technologiesService;
 
-
-    public User addNewUser(Integer telegramId) {
+    public User addUserAndSetFirstStep(Integer telegramId) {
         User user = new User(telegramId);
         user.setStep(Step.START);
-        return userRepository.save(user);
+        return user;
     }
 
     public void removeUser(User user) {
@@ -37,5 +37,35 @@ public class UserService {
         specializationService.removeAllUserSpecialisations(user);
         locationRepository.deleteById( user.getId() );
         userRepository.deleteById(user.getId());
+    }
+
+    @Override
+    public void setCity(User user, City city) {
+        user.setCity( city );
+    }
+
+    @Override
+    public void setCandidate(User user, String text) {
+        user.setName( text );
+    }
+
+    @Override
+    public void setEnglishLevel(User user, EnglishLevel englishLevel) {
+        user.setEnglishLevel( englishLevel );
+    }
+
+    @Override
+    public void setExperience(User user, Experience experience) {
+        user.setExperience( experience );
+    }
+
+    @Override
+    public void setSalaryUpTo(User user, Integer price) {
+        user.setSalaryUpTo( price );
+    }
+
+    @Override
+    public void setSalaryFrom(User user, Integer price) {
+        user.setSalaryFrom( price );
     }
 }
